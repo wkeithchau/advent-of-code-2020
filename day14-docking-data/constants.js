c45631c3a5f08c5bb708e502011f7d33f0ff4564
Keith Chau <wkeith.chau@gmail.com>
2021-12-03 23:03:18 +1030

    Complete day14 puzzles

---
+++ day14-docking-data/constants.js
@@ -0,0 +1,13 @@
+export const symbols = {
+    equals: ' = ',
+    frontBracket: '[',
+    backBracket: ']',
+    x: 'X',
+    zero: '0',
+    one: '1',
+}
+
+export const types = {
+    mask: 'mask',
+    mem: 'mem',
+}
